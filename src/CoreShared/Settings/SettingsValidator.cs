6a40b44f142f6f5923e2a885df9b747b46f907ae
Adrian Środoń <adriansrodon@tuta.io>
2025-11-01 18:40:39 +0100

    fix: .env variables validator

--- src/CoreShared/Settings/SettingsValidator.cs
+++ src/CoreShared/Settings/SettingsValidator.cs
@@ -12,15 +12,37 @@ public static class SettingsValidator
         return settings;
     }
 
-    // TODO: this is not working
     private static void ExecuteValidation(object settings)
     {
+        ArgumentNullException.ThrowIfNull(settings);
+        
         foreach (PropertyInfo prop in settings.GetType().GetProperties())
         {
+            if (prop.GetIndexParameters().Length > 0)
+                continue;
+            
+            var value = prop.GetValue(settings);
             var type = Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType;
             
             if (type == typeof(string))
-                ArgumentException.ThrowIfNullOrWhiteSpace(prop.GetValue(settings) as string);
+                ArgumentException.ThrowIfNullOrWhiteSpace(prop.GetValue(settings) as string, prop.Name);
+            else if (!type.IsValueType)
+            {
+                if (IsNullable(prop))
+                    continue;
+                
+                if (value is null)
+                    ArgumentNullException.ThrowIfNull(prop.GetValue(settings), prop.Name);
+                
+                ExecuteValidation(value!);
+            }
         }
+    }
+
+    private static bool IsNullable(PropertyInfo prop)
+    {
+        return prop.CustomAttributes
+            .FirstOrDefault(a => a.AttributeType.FullName == "System.Runtime.CompilerServices.NullableAttribute")
+            is not null;
     }
 }
