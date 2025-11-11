2ac238776c5b27fcc958c63d195b8f7a331fca03
Adrian Środoń <adriansrodon@tuta.io>
2025-11-11 12:35:11 +0100

    fix: notification-service - more logging

--- src/NotificationService/Services/EmailService.cs
+++ src/NotificationService/Services/EmailService.cs
@@ -4,7 +4,7 @@ using MimeKit.Text;
 
 namespace NotificationService.Services;
 
-public class EmailService
+public class EmailService(ILogger<EmailService> logger)
 {
     public async Task SendEmailAsync(
         string receiverName,
@@ -13,19 +13,29 @@ public class EmailService
         string bodyHtml,
         CancellationToken cancellationToken = default)
     {
-        MimeMessage message = new();
-        message.From.Add(new MailboxAddress("Papercut", "mail@papercut.com"));
-        message.To.Add(new MailboxAddress(receiverName, receiverEmail));
-        message.Subject = subject;
-
-        message.Body = new TextPart(TextFormat.Html)
+        try
         {
-            Text = bodyHtml
-        };
+            MimeMessage message = new();
+            message.From.Add(new MailboxAddress("Papercut", "mail@papercut.com"));
+            message.To.Add(new MailboxAddress(receiverName, receiverEmail));
+            message.Subject = subject;
 
-        var client = new SmtpClient();
-        await client.ConnectAsync("localhost", 25, false, cancellationToken);
-        await client.SendAsync(message, cancellationToken);
-        await client.DisconnectAsync(true, cancellationToken);
+            message.Body = new TextPart(TextFormat.Html)
+            {
+                Text = bodyHtml
+            };
+
+            using var client = new SmtpClient();
+            await client.ConnectAsync("localhost", 25, false, cancellationToken);
+            await client.SendAsync(message, cancellationToken);
+            await client.DisconnectAsync(true, cancellationToken);
+
+            logger.LogInformation("Email successfully sent to {receiverEmail}", receiverEmail);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error while sending email to {receiverEmail}", receiverEmail);
+            throw;
+        }
     }
 }
